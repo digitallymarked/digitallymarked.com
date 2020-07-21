1d95c297e22e506de84e5676eedd869c3d0c0140
Mark Markov <digitallymarked@gmail.com>
2020-07-21 21:12:42 +0300

    Added Site Metadata as a hook

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,11 @@
  */
 
 module.exports = {
+  siteMetadata: {
+    title: `DigitallyMarked - Digital Space of Mark Markov`,
+    description: `
+      This is the personal digital space of Mark Markov. You can find both fun and quirky stuff as well as some more serious and inspirational ones.
+    `,
+  },
   plugins: [`gatsby-plugin-styled-components`, `gatsby-plugin-react-helmet`],
 }
--- src/theme/Head.js
+++ src/theme/Head.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
+import { useSiteMetadata } from "../hooks/use-site-metadata"
 
-const Head = () => (
-  <Helmet>
-    <title>Personal website of Mark Markov</title>
-    <link href="https://fonts.googleapis.com/css2?family=Halant:wght@300;400;500;600;700&family=Nunito+Sans:ital,wght@0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"></link>
-  </Helmet>
-)
-
-export default Head
+export default function Head () {
+  const { title, description } = useSiteMetadata()
+  return (
+    <Helmet>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Halant:wght@300;400;500;600;700&family=Nunito+Sans:ital,wght@0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet"
+      ></link>
+    </Helmet>
+  )
+}
