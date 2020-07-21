549fca49a52affeb83f26c4196ff0c197383f331
Mark Markov <digitallymarked@gmail.com>
2020-07-22 01:44:35 +0300

    Laid out components for better import/export

--- src/theme/Layout.js
+++ src/theme/Layout.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { createGlobalStyle } from 'styled-components'
 
-import Navigation from '../components/Navigation'
+import { Navigation } from '../components/Navigation'
 
 import { PrefersReducedMotion, SiteResets, Typography } from '../styles/base'
 
@@ -14,7 +14,7 @@ const GlobalStyle = createGlobalStyle`
 const Layout = ({ children }) => (
   <div className="layout">
     <GlobalStyle />
-    <Navigation/>
+    <Navigation />
     {children}
   </div>
 )
